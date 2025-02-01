dc59273db335cffe0019633ab10a2daa6366bea6
Thomas Pranic <tjpranic@gmail.com>
2025-02-01 13:51:18 +1100

    Spacing

--- svelte.config.js
+++ svelte.config.js
@@ -4,15 +4,15 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type { import( '@sveltejs/kit' ).Config } */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: vitePreprocess( ),
-  kit: {
-    adapter: adapter( ),
-    alias: {
-      '$routes': 'src/routes',
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: vitePreprocess( ),
+    kit: {
+        adapter: adapter( ),
+        alias: {
+            '$routes': 'src/routes',
+        }
     }
-  }
 };
 
 export default config;
